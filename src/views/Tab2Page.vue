d1b4d4c99a0fdad4c3150ca6ff7d65d4bb93372c
Michał Ponichtera <m.ponichtera@student.po.edu.pl>
2022-11-08 18:03:45 +0100

    added qr code support

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -1,3 +1,75 @@
+<script setup>
+import {StreamBarcodeReader} from 'vue-barcode-reader';
+import {modalController,alertController} from "@ionic/vue"
+import MaszynaModal from "@/components/MaszynaModal.vue";
+import { ref,defineEmits} from "vue";
+
+const emit = defineEmits([
+  "update:modelValue",
+  "change",
+])
+const idMaszyny=ref("")
+const updateIdMaszyny = ($event) => {
+  idMaszyny.value = $event.target.value
+    emit("update:idMaszyny", $event.target.value)
+}
+
+async function openMaszynaModal(passedMaszyna){
+  const modal = await modalController.create({
+    component: MaszynaModal, 
+    componentProps:{
+      idMaszyny:passedMaszyna
+    }
+  });
+  return modal.present();
+}
+
+const onLoaded = () => {
+  console.log("loaded")
+}
+
+const customAlert = async (alertType) => {
+  let header =""
+  if(alertType=="machineNotFound"){
+    header="Nie znaleziono maszyny o podanym id. Proszę podać prawidłowe id maszyny."
+  }
+  else if(alertType=="wrongQrCode"){
+    header="Zeskanowano nieprawidłowy kod QR. Proszę zeskanować prawidłowy kod lub podać id maszyny."
+  }
+  const alert=await alertController.create({
+      header:header,
+      buttons:[
+        {
+          text: 'Ok',
+          role: 'cancel',
+          handler: () => {
+              },
+        },
+      ]
+  })
+  await alert.present();
+};
+
+const onDecode = (text) => {
+  console.log("decoded");
+  console.log(text);
+  if(text.split(":")[0]=="idMaszyny"){
+    fetch(`https://localhost:7023/api/maszyny/${text.split(":")[1]}`, {method:'GET',mode:'cors'}).then((response)=>{
+      if(response.status===404||response.status===400){
+        customAlert("machineNotFound")
+      }
+      else{
+        openMaszynaModal(text.split(":")[1])
+      }
+    })
+  }
+  else{
+    customAlert("wrongQrCode")
+  }
+}
+
+</script>
+
 <template>
   <ion-page>
     <top-toolbar pageName="Skaner"/>
@@ -7,19 +79,39 @@
           <ion-title size="large">Skaner</ion-title>
         </ion-toolbar>
       </ion-header>
-      
-     
+      <ion-card>
+        <ion-card-header style="text-align:center" >
+          <ion-label><h2>Zeskanuj kod QR maszyny:</h2></ion-label>
+        </ion-card-header>
+        <ion-card-content>
+          <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded"></StreamBarcodeReader>
+        </ion-card-content>
+        <ion-card-header style="text-align:center" >
+          <ion-label><h2>Wpisz id maszyny:</h2></ion-label>
+        </ion-card-header>
+        <ion-card-content>
+          
+          <ion-list style="text-align:center" >
+            <ion-input placeholder="#id" :value="idMaszyny" @input="updateIdMaszyny" @change="$emit('change',$event.target.value)"></ion-input>
+            <ion-button expand="block" fill="clear" color="transparent" @click="onDecode(('idMaszyny:').concat(idMaszyny))">Wyszukaj po id</ion-button>
+          </ion-list>
+          
+        </ion-card-content>
+        
+        
+      </ion-card>
+    
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonCard } from '@ionic/vue';
 
 
 export default defineComponent({
   name: 'Tab2Page',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonCard }
 });
 </script>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -4,17 +4,17 @@
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
         <ion-tab-button tab="tab1" href="/tabs/tab1">
-          <ion-icon :icon="triangle" />
+          <ion-icon :icon="constructOutline"/>
           <ion-label>Zlecenia</ion-label>
         </ion-tab-button>
           
         <ion-tab-button tab="tab2" href="/tabs/tab2">
-          <ion-icon :icon="ellipse" />
+          <ion-icon :icon="qrCodeOutline" />
           <ion-label>Skanuj maszynę</ion-label>
         </ion-tab-button>
         
         <ion-tab-button tab="tab3" href="/tabs/tab3">
-          <ion-icon :icon="square" />
+          <ion-icon :icon="personCircleOutline" />
           <ion-label>Profil</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
@@ -25,16 +25,16 @@
 <script>
 import { defineComponent } from 'vue';
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet } from '@ionic/vue';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { personCircleOutline,qrCodeOutline, constructOutline } from 'ionicons/icons';
 
 export default defineComponent({
   name: 'TabsPage',
   components: { IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet },
   setup() {
     return {
-      ellipse, 
-      square, 
-      triangle,
+      personCircleOutline, 
+      qrCodeOutline, 
+      constructOutline,
     }
   }
 });
